65f6a6c25b68a90fed58cbe3f3e3faae2701abd8
binov-com <bessam.benyahya@gmail.com>
2017-05-11 12:50:16 +0200

    Api create ProductResource and CategoryResource.

---
+++ Controllers/Resources/CategoryResource.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace bdeliv_services.Controllers.Resources
+{
+    public class CategoryResource
+    {
+        public int Id { get; set; }
+
+        public bool Status { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Tax { get; set; }
+
+        public string ImgPath { get; set; }
+
+        public string ImgName { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public ICollection<ProductResource> Products { get; set; }
+
+        public CategoryResource()
+        {
+            Products = new Collection<ProductResource>();
+        }
+    }
+}
---
+++ Controllers/Resources/ProductResource.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace bdeliv_services.Controllers.Resources
+{
+    public class ProductResource
+    {
+        public int Id { get; set; }
+
+        public bool Status { get; set; }
+
+        public string Reference { get; set; }
+
+        public string Name { get; set; }
+
+        public string Origin { get; set; }
+
+        public decimal Weight { get; set; }
+
+        public string Measure { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal Tax { get; set; }
+
+        public string ImgName { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
