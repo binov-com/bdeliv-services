19748d7c6325a5a29e27f386711ddbd9b8260d30
binov-com <bessam.benyahya@gmail.com>
2017-05-11 15:19:03 +0200

    Database Table Users Seed Data.

---
+++ Migrations/20170511131309_SeedTableUsersWithDatas.Designer.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using bdeliv_services.Persistence;
+
+namespace BdelivServices.Migrations
+{
+    [DbContext(typeof(BdelivDbContext))]
+    [Migration("20170511131309_SeedTableUsersWithDatas")]
+    partial class SeedTableUsersWithDatas
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("bdeliv_services.Models.Category", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("CreatedAt");
+
+                    b.Property<string>("ImgName")
+                        .HasMaxLength(512);
+
+                    b.Property<string>("ImgPath")
+                        .HasMaxLength(128);
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(128);
+
+                    b.Property<bool>("Status");
+
+                    b.Property<decimal>("Tax");
+
+                    b.Property<DateTime>("UpdatedAt");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Categories");
+                });
+
+            modelBuilder.Entity("bdeliv_services.Models.Product", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("CategoryId");
+
+                    b.Property<DateTime>("CreatedAt");
+
+                    b.Property<string>("ImgName")
+                        .HasMaxLength(128);
+
+                    b.Property<string>("Measure")
+                        .HasMaxLength(32);
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(128);
+
+                    b.Property<string>("Origin")
+                        .HasMaxLength(128);
+
+                    b.Property<decimal>("Price");
+
+                    b.Property<int>("Quantity");
+
+                    b.Property<string>("Reference")
+                        .IsRequired()
+                        .HasMaxLength(128);
+
+                    b.Property<bool>("Status");
+
+                    b.Property<decimal>("Tax");
+
+                    b.Property<DateTime>("UpdatedAt");
+
+                    b.Property<decimal>("Weight");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("CategoryId");
+
+                    b.ToTable("Products");
+                });
+
+            modelBuilder.Entity("bdeliv_services.Models.User", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Comment")
+                        .HasMaxLength(1024);
+
+                    b.Property<DateTime>("CreatedAt");
+
+                    b.Property<string>("Email")
+                        .HasMaxLength(128);
+
+                    b.Property<string>("FirstName")
+                        .IsRequired()
+                        .HasMaxLength(128);
+
+                    b.Property<string>("Gender")
+                        .HasMaxLength(8);
+
+                    b.Property<string>("Hash")
+                        .HasMaxLength(256);
+
+                    b.Property<bool>("IsAdmin");
+
+                    b.Property<bool>("IsDelivery");
+
+                    b.Property<DateTime>("LastLoginAt");
+
+                    b.Property<string>("LastName")
+                        .IsRequired()
+                        .HasMaxLength(128);
+
+                    b.Property<string>("Mobile")
+                        .HasMaxLength(16);
+
+                    b.Property<string>("Password")
+                        .HasMaxLength(256);
+
+                    b.Property<string>("Phone")
+                        .HasMaxLength(16);
+
+                    b.Property<bool>("Status");
+
+                    b.Property<DateTime>("UpdatedAt");
+
+                    b.Property<bool>("Valid");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Users");
+                });
+
+            modelBuilder.Entity("bdeliv_services.Models.Product", b =>
+                {
+                    b.HasOne("bdeliv_services.Models.Category", "Category")
+                        .WithMany("Products")
+                        .HasForeignKey("CategoryId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
---
+++ Migrations/20170511131309_SeedTableUsersWithDatas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BdelivServices.Migrations
+{
+    public partial class SeedTableUsersWithDatas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string dtNow = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+            migrationBuilder.Sql(
+                "INSERT INTO Users (CreatedAt, Email, FirstName, Gender, IsAdmin, LastLoginAt, LastName, Mobile, Phone, Status, UpdatedAt, IsDelivery, Valid) " +
+                "VALUES ('" + dtNow + "', 'bessam.benyahya@gmail.com', 'Bessam', 'Mr', 1, '" + dtNow + "', 'BEN YAHYA', '06 01 01 01 01', '01 01 01 01 01', 1, '" + dtNow + "', 1, 0)"
+            );
+
+            migrationBuilder.Sql(
+                "INSERT INTO Users (CreatedAt, Email, FirstName, Gender, IsAdmin, LastLoginAt, LastName, Mobile, Phone, Status, UpdatedAt, IsDelivery, Valid) " +
+                "VALUES ('" + dtNow + "', 'slesage@bdeliv.com', 'Sophie', 'Mme', 1, '" + dtNow + "', 'LESAGE', '06 01 01 01 01', '01 01 01 01 01', 1, '" + dtNow + "', 0, 0)"
+            );
+
+            migrationBuilder.Sql(
+                "INSERT INTO Users (CreatedAt, Email, FirstName, Gender, IsAdmin, LastLoginAt, LastName, Mobile, Phone, Status, UpdatedAt, IsDelivery, Valid) " +
+                "VALUES ('" + dtNow + "', 'ncherdoud@gmail.com', 'Nora', 'Mme', 0, '" + dtNow + "', 'CHERDOUD', '06 01 01 01 01', '01 01 01 01 01', 1, '" + dtNow + "', 1, 0)"
+            );
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "DELETE FROM Users " +
+                "WHERE Telephone = '01 01 01 01 01'"
+                );
+        }
+    }
+}
