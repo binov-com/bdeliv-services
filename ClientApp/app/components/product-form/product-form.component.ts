322bbe13ca94070245e742d81ea0001714005a84
binov-com <bessam.benyahya@gmail.com>
2017-05-12 11:17:09 +0200

    Angular Product Populating Drop-Down List with CategorieService.

--- ClientApp/app/components/product-form/product-form.component.ts
+++ ClientApp/app/components/product-form/product-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategorieService } from "../../services/categorie.service";
 
 @Component({
   selector: 'app-product-form',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-form.component.css']
 })
 export class ProductFormComponent implements OnInit {
+  categories;
 
-  constructor() { }
+  constructor(private categorieService: CategorieService) { }
 
   ngOnInit() {
+    this.categorieService.getCategories().subscribe(
+      categories => {
+        this.categories = categories;
+        console.log("CATEGORIES", this.categories);
+      }
+    );
+
+    
+
   }
 
 }
