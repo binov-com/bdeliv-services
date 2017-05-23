c1c2ac7854760e18fe8ff58265a996fed1282dcf
binov-com <bessam.benyahya@gmail.com>
2017-05-23 21:07:19 +0200

    API Repository Pattern Add ProductRepository (GetProduct).

---
+++ Persistence/ProductRepository.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using bdeliv_services.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace bdeliv_services.Persistence
+{
+    public class ProductRepository
+    {
+        private readonly BdelivDbContext context;
+        public ProductRepository(BdelivDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Product> GetProduct(int id)
+        {
+            return await context.Products
+                .Include(t => t.Tags)
+                    .ThenInclude(pt => pt.Tag)
+                .Include(t => t.Category)
+                .SingleOrDefaultAsync(t => t.Id == id);
+        }
+    }
+}
