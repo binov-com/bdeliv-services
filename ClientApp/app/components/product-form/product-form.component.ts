e23557f7ee30886a5b1c9520543bc5a550b8a1e0
binov-com <bessam.benyahya@gmail.com>
2017-05-31 15:31:34 +0200

    NPM | Webpack Install NG2-TOASTY.

--- ClientApp/app/components/product-form/product-form.component.ts
+++ ClientApp/app/components/product-form/product-form.component.ts
@@ -44,7 +44,12 @@ export class ProductFormComponent implements OnInit {
 
   submit() {
     this.productService.create(this.product)
-      .subscribe(x => console.log(x));
+      .subscribe(
+        x => console.log(x),
+        err => {
+            console.log(err.status)
+          }
+        );
   }
 
 }
