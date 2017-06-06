d12b9c8902d529d1101519ea89137f87d4f81b5b
binov-com <bessam.benyahya@gmail.com>
2017-06-06 17:38:20 +0200

    Angular | Product | Product List - Adding the Filters.

--- ClientApp/app/components/product-list/product-list.component.ts
+++ ClientApp/app/components/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from "../../services/product.service";
 
 import { Product } from "../../models/product";
+import { Category } from "../../models/category";
 
 @Component({
   selector: 'app-product-list',
@@ -9,13 +10,21 @@ import { Product } from "../../models/product";
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  categories: Category[];
   products: Product[];
-
+  filter: any = {};
+  
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.productService.getCategories()
+      .subscribe(categories => this.categories = categories);
+
     this.productService.getProducts()
       .subscribe(products => this.products = products);
   }
 
+  onFilterChange() {
+
+  }
 }
