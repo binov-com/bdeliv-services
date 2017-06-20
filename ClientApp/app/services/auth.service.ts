e9a5326c1b3694a8cb31491f114ddd3437d26df9
binov-com <bessam.benyahya@gmail.com>
2017-06-20 14:58:15 +0200

    Angular | Authentication | Auth0 - Get the User's Profile.

--- ClientApp/app/services/auth.service.ts
+++ ClientApp/app/services/auth.service.ts
@@ -6,6 +6,8 @@ import * as auth0 from 'auth0-js';
 @Injectable()
 export class AuthService {
 
+  profile: any;
+
   auth0 = new auth0.WebAuth({
     clientID: 'MgNB9xdWAs06k31QJq8gjEv2Xe06WoYH',
     domain: 'bdeliv.auth0.com',
@@ -13,10 +15,12 @@ export class AuthService {
     audience: 'https://bdeliv.auth0.com/userinfo',
     redirectUri: 'http://localhost:5000/home',      
     //redirectUri: 'http://localhost:4200/callback',      
-    scope: 'openid'
+    scope: 'openid profile'
   });
 
-  constructor(public router: Router) {}
+  constructor(public router: Router) {
+    this.profile = JSON.parse(localStorage.getItem('profile'));
+  }
 
   public login(): void {
     this.auth0.authorize();
@@ -42,7 +46,13 @@ export class AuthService {
     //localStorage.setItem('id_token', authResult.idToken);
     localStorage.setItem('expires_at', expiresAt);
 
-    
+    this.auth0.client.userInfo(authResult.accessToken, (err, profile) => {
+      if(err)
+        throw err;
+      
+      localStorage.setItem('profile', JSON.stringify(profile));
+      this.profile = profile;
+    });
   }
 
   public logout(): void {
@@ -50,6 +60,8 @@ export class AuthService {
     localStorage.removeItem('access_token');
     //localStorage.removeItem('id_token');
     localStorage.removeItem('expires_at');
+    localStorage.removeItem('profile');
+    this.profile = null;
     // Go back to the home route
     this.router.navigate(['/']);
   }
